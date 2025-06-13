cbb4a271135de6427b532f782c0ca98e37e22e8a
kjhoon <kjhooni@innogrid.com>
2025-06-13 00:16:10 +0000

    remove vso feature and related code

--- src/main/java/org/container/platform/web/ui/managements/secrets/SecretsController.java
+++ src/main/java/org/container/platform/web/ui/managements/secrets/SecretsController.java
@@ -29,16 +29,6 @@ public class SecretsController {
     }
 
     /**
-     * Vault Secrets 목록 페이지 이동(Go to the vault secrets list page)
-     *
-     * @return the view
-     */
-    @GetMapping(value = ConstantsUrl.URI_CP_CONFIGS_VAULT_SECRETS)
-    public String getVaultSecretsList() {
-        return BASE_URL + "vaultSecrets";
-    }
-
-    /**
      * Secrets 상세 페이지 이동(Go to the secrets details page)
      *
      * @return the view
@@ -46,16 +36,6 @@ public class SecretsController {
     @GetMapping(value = ConstantsUrl.URI_CP_CONFIGS_SECRETS + ConstantsUrl.URI_CP_DETAILS)
     public String getSecretsDetails() {
         return BASE_URL + "secretsDetail";
-    }
-
-    /**
-     * Vault Secrets 상세 페이지 이동(Go to the vault secrets details page)
-     *
-     * @return the view
-     */
-    @GetMapping(value = ConstantsUrl.URI_CP_CONFIGS_SECRETS + ConstantsUrl.URI_CP_VAULT_DETAILS)
-    public String getVaultSecretsDetails() {
-        return BASE_URL + "vaultSecretsDetail";
     }
 
     /**
